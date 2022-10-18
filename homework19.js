f28d3478925eb79f43c3cc03f6d8501c10fda281
Post-Nataly <post-design@mail.ru>
2022-10-18 22:31:41 +0800

    2

--- homework19.js
+++ homework19.js
@@ -7,5 +7,23 @@ let sum = (parseInt(a.slice(1)) + parseInt(b.slice(0,3)));
 
 console.log(sum);
 
-//2
+// 2
 
+let message = ' привет, медвед      ';
+
+message = message.trim();
+
+console.log(message[0].toUpperCase() + message.slice(1));
+
+// 3
+
+let age = prompt("Сколько Вам лет?");
+
+switch (age) {
+    case 2:
+        alert(`Вы младенец и вам {age} лет`);
+        break;
+
+    default:
+        break;
+}
