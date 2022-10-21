406891a969ed2896026c6b7600e6a3b3df243004
Post-Nataly <post-design@mail.ru>
2022-10-21 19:38:13 +0800

    correction hw-19

--- homework19.js
+++ homework19.js
@@ -15,7 +15,7 @@ let message = ' привет, медвед      ';
 
 message = message.trim();
 
-console.log(message[0].toUpperCase() + message.slice(1));
+console.log(`"${message[0].toUpperCase() + message.slice(1)}"`);
 
 
 
@@ -26,16 +26,16 @@ let age = prompt("Сколько Вам лет?");
 if (age > 0 && age <= 3){
     alert(`Вам ${age} лет и вы младенец`)
 }
-else if (age >= 4 && age <= 11){
+else if (age <= 11){
     alert(`Вам ${age} лет и вы ребенок`)
 }
-else if (age >= 12 && age <= 18){
+else if (age <= 18){
     alert(`Вам ${age} лет и вы подросток`)
 }
-else if (age >= 19 && age <= 40){
+else if (age <= 40){
     alert(`Вам ${age} лет и вы познаёте жизнь`)
 }
-else if (age >= 41 && age <= 80){
+else if (age <= 80){
     alert(`Вам ${age} лет и вы познали жизнь`)
 }
 else if (age >= 81){
