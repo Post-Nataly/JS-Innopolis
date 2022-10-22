559bcdb4b507bd21cbe0b01b5d0a13e7374ca7fc
Post-Nataly <post-design@mail.ru>
2022-10-22 18:59:39 +0800

    hw20-2

--- homework20.js
+++ homework20.js
@@ -9,4 +9,20 @@ while (i <= 20) {
 
 
 
- 
+// 2
+
+let sum = 0;
+
+for (let count = 0; count < 3; count++) {
+
+    let num = +prompt("Введите число:");
+
+    if (!num) {
+        alert("Ошибка, Вы ввели не число!");
+    }
+    else {
+        sum += num;
+    }
+}
+
+alert("Сумма введённых чисел: " + sum);
