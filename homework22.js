0c351c01a7b6f200cf4e693918307f3cded2f149
Post-Nataly <post-design@mail.ru>
2022-10-30 17:42:30 +0800

    hw22-3.

--- homework22.js
+++ homework22.js
@@ -6,7 +6,7 @@ function getSumm(arr) {
     let sum = 0;
     for (let index = 0; index < arr.length; index++) {
         if (Number(arr[index])) {
-            sum += arr[index];            
+            sum += arr[index];
         } else {
             continue;
         }
@@ -14,7 +14,7 @@ function getSumm(arr) {
     return sum;
 }
 
-let arr1 = ["f", 5, "abc", 6, "k", 2];
+let arr1 = ["f", 15, "abc", 6, "k", 2];
 
 alert(getSumm(arr1));
 
@@ -23,6 +23,40 @@ alert(getSumm(arr1));
 // 3
 
 let cart = [4884];
-cart.push(3456);
-console.log(cart);
 
+function addToCart(arr) {
+    let newarr = [];
+    for (let index = 0; index < arr.length; index++) {
+        if (!newarr.includes(arr[index])) {
+            newarr.push(arr[index]);
+        }
+        else {
+            continue;
+        }
+    }
+    return newarr;
+}
+
+cart.push(5525);
+
+cart.push(3446);
+
+cart.push(5525);
+
+console.log(addToCart(cart));
+
+function removeFromCart(arr) {
+    let newarr = [];
+    for (let index = 0; index < arr.length; index++) {
+        if (newarr.includes(arr[index])) {
+            delete arr[index];
+        } else {
+            continue;
+        }
+    }
+    return newarr;
+}
+
+cart.delete(5525);
+
+console.log(removeFromCart(cart));
